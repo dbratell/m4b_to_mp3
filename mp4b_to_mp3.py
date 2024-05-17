4c585444c1ca330a5d07f6691f30b6cb7215b573
Daniel Bratell <bratell@lysator.liu.se>
2024-05-17 17:51:51 +0200

    Be a bit more generous with argument interpretation
    Allow the user to specify a file and set
    a useful default for target_dir

--- mp4b_to_mp3.py
+++ mp4b_to_mp3.py
@@ -8,20 +8,31 @@ from pydub import AudioSegment
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--target-dir")
+    parser.add_argument("--target-dir", help="Defaults to the source directory")
     parser.add_argument("--skip-existing", action="store_true",
             help="Don't convert files if the target file already exists.")
-    parser.add_argument("source_dir", nargs="?", default=".")
-                        
+    parser.add_argument("source_dir_or_file", nargs="?", default=".",
+                        help="Directory with mp4b files, defaults to the current directory.")
+
     args = parser.parse_args()
-    source_files = glob.glob("*.m4b", root_dir=args.source_dir)
-    if not source_files:
-        print("No m4b files found in %s" % args.source_dir)
+    if os.path.isfile(args.source_dir_or_file):
+        print(f"{args.source_dir_or_file} is a file, assuming it is an audio file")
+        source_files = [args.source_dir_or_file]
+        source_dir = os.path.dirname(args.source_dir_or_file)
+    else:
+        source_files = glob.glob("*.m4b", root_dir=args.source_dir_or_file)
+        if not source_files:
+            print("No m4b files found in %s" % args.source_dir_or_file)
+        source_dir = args.source_dir
+
+    target_dir = args.target_dir
+    if target_dir is None:
+        target_dir = source_dir
 
     for mp4b_file in source_files:
         base_name, _ext = os.path.splitext(mp4b_file)
         new_file_name = base_name + ".mp3"
-        target_file = os.path.join(args.target_dir, new_file_name)
+        target_file = os.path.join(target_dir, new_file_name)
         print("Converting %s..." % mp4b_file , end="")
         if args.skip_existing and os.path.isfile(target_file):
             print("Already Done")
@@ -30,8 +41,8 @@ def main():
         print("(loading)...", end="")
         sys.stdout.flush()
         mp4wav_audio = AudioSegment.from_file(
-            os.path.join(args.source_dir, mp4b_file), format="mp4")
-        print("(%.1fs)...(saving)..." % (time.time()- start_time), 
+            os.path.join(source_dir, mp4b_file), format="mp4")
+        print("(%.1fs)...(saving)..." % (time.time()- start_time),
             end="")
         sys.stdout.flush()
         start_time = time.time()
